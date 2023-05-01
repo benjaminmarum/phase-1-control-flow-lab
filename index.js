b9a8616f287ac6159009c3d604b3df8fe4cc1a4c
Ben <ben.j.marum@gmail.com>
2023-05-01 16:24:42 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+if (feet<400) {
+  return 'This one is on me!'
+} else if (feet<2000){
+  return 'That will be twenty bucks.'
+} else if (feet<=2500){
+  return 'I will gladly take your thirty bucks.'
+} else {
+  return 'No can do.'
+}};
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  const destination = city ==='NYC' ? 'Ok, sounds good.' : "No go.";
+  return destination;
 }
 
-function ternaryCheckCity(){
+let reply;
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch (tip) {
+    case 'generous':
+      reply = 'Thank you so much.';
+      return reply;
+    case 'not as generous':
+      reply = 'Thank you.';
+      return reply;
+    default:
+      reply = 'Bye.'
+      return reply;
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
